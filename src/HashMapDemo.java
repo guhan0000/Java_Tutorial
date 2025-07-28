b603077ae07edf7f5083dd1a3b2ff6b071d8d1ec
guhan0000 <guhanm0000@gmail.com>
2025-07-28 19:19:22 +0530

    commit_1

---
+++ src/HashMapDemo.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<String,Integer> empid=new HashMap();
+//		String string = null;
+//		System.out.println(string.length());
+		empid.put("Guhan", 681);
+		empid.put("Naresh", 685);
+		empid.put("Raghul", 684);
+		empid.put("Guhan",123);
+		empid.put("vasanth", 681);
+	System.out.println(empid.containsKey("vasanth"));	
+		System.out.println(empid.get("Guhan"));
+	System.out.println(empid.containsKey("Guhan"));	
+	empid.remove("vasanth");
+	System.out.println(empid);
+	empid.replace("Naresh", 685, 665);
+	empid.putIfAbsent("NARESH", 0);
+	for(String key :empid.keySet())
+	{
+		System.out.println(key+empid.get(key));
+	}
+	
+		
+		
+	}
+}
