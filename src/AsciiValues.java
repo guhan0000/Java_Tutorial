5af70080e0e12182560894f36d7534d5e672df26
guhan0000 <guhanm0000@gmail.com>
2025-07-28 18:24:29 +0530

    first commit

---
+++ src/AsciiValues.java
@@ -0,0 +1,11 @@
+
+public class AsciiValues {
+	 char ch='A';
+	 int num=97;
+	public static void main(String[] args) {
+		AsciiValues as=new AsciiValues();
+		System.out.println((int)as.ch+""+(char)as.num);
+		
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+
+	static void compute(int n)
+	{
+		int a=(int) -(Math.pow(2,(n-1)));
+		int b=(int)Math.pow(2, (n-1))-1;
+		System.out.println(a+"to"+b);
+	}
+	public static void main(String args[])
+	{
+		byte $age=-127;
+		//System.out.println(age);
+		boolean res=true;
+		short pin=1122;
+		int pincode=614630;
+		long phone=963258741000000000l;
+		float salary=11000.257f;
+		double loan=15000000.250;
+		char gender='m';
+		String name="Guhan";
+		char ch=name.charAt(0);
+		System.out.println($age);
+		
+		System.out.println(name.contains("uan"));
+		
+		
+		compute(8);
+	}
+}
