17e777ffc97486e12b3d3ecf0421ce089ca44c9c
guhan0000 <guhanm0000@gmail.com>
2025-11-14 11:44:30 +0530

    solved

--- src/org/arrays/RemoveDuplicateArray.java
+++ src/org/arrays/RemoveDuplicateArray.java
@@ -3,27 +3,30 @@ public class RemoveDuplicateArray {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3,4,4,5};
         int[] temp = new int[arr.length];
-        int j = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            boolean found = false;
-
-            // check if arr[i] already exists in temp
-            for (int k = 0; k < j; k++) {
-                if (arr[i] == temp[k]) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                temp[j++] = arr[i];
-            }
-        }
-
-        // print result
-        for (int i = 0; i < j; i++) {
-            System.out.print(temp[i] + " ");
-        }
+        int j=0;
+        
+      for(int i=0;i<arr.length;i++)
+      {
+      	boolean found=false;
+      	for(int k=0;k<j;k++)
+      	{
+      		if(arr[i]==temp[k])
+      		{
+      			found=true;
+      			break;
+      		}
+      	}
+      	if(!found)
+      	{
+      		temp[j++]=arr[i];
+      	}
     }
-}
+     for(int i=0;i<j;i++)
+     {
+    	 System.out.println(temp[i]);
+     }
+       
+        	}
+        }
+
+    
