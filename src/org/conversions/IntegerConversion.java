c8eea50dc6c584ae6424df73b9544fc99c7acb9e
guhan0000 <guhanm0000@gmail.com>
2026-02-21 23:41:44 +0530

    integer conversions method added

---
+++ src/org/conversions/IntegerConversion.java
@@ -0,0 +1,9 @@
+package org.conversions;
+
+public class IntegerConversion {
+	public static void main(String[] args) {
+		int int1 = Integer.parseInt("01");
+		System.out.println(int1);
+		System.out.println();
+	}
+}
